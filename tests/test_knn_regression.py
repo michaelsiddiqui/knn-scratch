e41932abb3ecaa3d449041ddf75f2ae338cf70d2
Michael Siddiqui <siddiqui@alumni.upenn.edu>
2019-02-16 16:52:55 -0800

    add TestCase class with tests for predict_knn_regression_for_set function

--- tests/test_knn_regression.py
+++ tests/test_knn_regression.py
@@ -7,6 +7,7 @@ to contain a test class per class or function in the knn_regression module
 import unittest
 
 from knn_regression import predict_from_knn_average
+from knn_regression import predict_knn_regression_for_set
 
 FIXTURE_DATASET1 = [
     [-1, 0, 0, 'out', 1.0],
@@ -33,4 +34,23 @@ class TestPredictFromKnnAverage(unittest.TestCase):
                                           3,
                                           category_index=4)
         expected = 3.0
+        self.assertEqual(actual, expected)
+
+
+class Test(unittest.TestCase):
+    """TestCase class containing tests for predict_knn_regression_for_set
+    """
+    def test_simple_query_set_and_model_dataset_result(self):
+        """query a few rows against fixture dataset1 get expected precitions
+        """
+        dataset = FIXTURE_DATASET1
+        query_set = [[6, 6, 6], [4, 4, 4], [-1, -1, -1]]
+        expected = [3.5, 2.5, 1.0]
+        actual = predict_knn_regression_for_set(
+            query_set,
+            dataset,
+            2,
+            3,
+            category_index=4
+        )
         self.assertEqual(actual, expected)
